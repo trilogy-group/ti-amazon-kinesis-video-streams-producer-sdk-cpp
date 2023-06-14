ca17413a9777cc81ff1dab11cbf93450956ef56b
Aashish Gaba <aashish.gaba@codenation.co.in>
2023-06-14 13:09:21 +0000

    update stream local video script

--- stream_local_video.sh
+++ stream_local_video.sh
@@ -1,2 +1,2 @@
 cd build
-./kvs_gstreamer_sample agaba-merlyn-test ../videos/workshop_sample.mp4
+while true; do ./kvs_gstreamer_sample agaba-merlyn-test ../videos/workshop_sample.mp4 && sleep 10s; done
