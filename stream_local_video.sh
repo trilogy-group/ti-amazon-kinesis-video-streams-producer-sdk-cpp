281967f7191f9bcd6f2a4c8a957b1a4e7f6d2f0f
Aashish Gaba <aashish.gaba@codenation.co.in>
2023-06-26 07:24:09 +0000

    Add yt github group video to the set of videos

--- stream_local_video.sh
+++ stream_local_video.sh
@@ -1,2 +1,2 @@
 cd build
-while true; do ./kvs_gstreamer_sample agaba-merlyn-test ../videos/workshop_sample.mp4 && sleep 10s; done
+while true; do ./kvs_gstreamer_sample agaba-merlyn-test ../videos/group_video.mp4 && sleep 10s; done
